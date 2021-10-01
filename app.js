36cf16ef7e5bace5e805e438ec8c43f6399e8cbf
Ian <ianmat55@gmail.com>
2021-09-30 20:23:45 -0700

    decided on postgreSQL for a database and ejs for templating until react is learned

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require('express');
 const app = express();
 const path = __dirname;
 
-const port = 3000;
+const port = process.env.port || 3000;;
 
 // Static Files
-app.use(express.static('public'));
-app.use('/css', express.static(path + 'public/css'));
-app.use('/js', express.static(path + 'public/js'));
+app.use(express.static(path + 'public'));
 app.use('/img', express.static(path + 'public/img'));
 
 // Set Views
+app.set('view engine', 'ejs');
 
 // home
 app.get('/', (req, res) => {
-	res.sendFile(path + '/views/index.html')
+	res.render('index', { title:"Hire.me" });
 });
+
+app.get('/apps', (req, res) => {
+	res.render('applications', { title: "manage_apps" });
+})
 
 // 404 page
 app.use((req, res) => {
-	res.sendFile(path + '/views/404.html');
+	res.render('404', { title: 404 });
 })
 
 app.listen(port, () => {
--- indeed_scraper.js
+++ indeed_scraper.js
@@ -1,28 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const job_board = ["https://www.indeed.com/", "https://www.linkedin.com/jobs/", "https://www.builtinsf.com/jobs"];
 const keywords = ['python', 'junior', 'flask'];
 const jobs = {};
 const avoid = ['revature'];
 
 // axios response element schema
 // response = {data, status, statusText, headers, config} 
+async function getJobListings() {
+	const { data } = await axios.get(`https://www.indeed.com/jobs?q=${keywords[1]}%20developer%20${keywords[0]}&l=Daly%20City%2C%20CA&vjk`);
+	const labels = ['id', 'title', 'company', 'location', 'description', 'link', 'date'];
+	const identifiers = ['.jobTitle', '.companyName', 'companyLocation', '.job-snippet', 'href', 'date'];
+	const $ = cheerio.load(data);
+	const listingTable =  $('.mosaic-provider-jobcards');
 
-axios.get(`https://www.indeed.com/jobs?q=${keywords[1]}%20developer%20${keywords[0]}&l=Daly%20City%2C%20CA&vjk`)
-	.then((response) => {
-		let $ = cheerio.load(response.data);
-
-		// can use .tex(), .html(), .find(), children(), parent() on object
-		$('.result').each((index, element) => {
-			// job title = <h2 class="jobTitle">
-			// company name = <span class="companyName">
-			// location = <div class=companyLocation>
-			// description = <div class="job-snippet"> --> ul --> li
-			// job link = <a class="tapItem fs-unmast result">
-			// date = <span class="date">
-
-			const title = $(element)
+	// can use .text(), .html(), .find(), children(), parent() on object
+	listingTable.find('.result').each((i, element) => {
+		const title = $(element)
 				.find('.jobTitle')
 				.text();
 				
@@ -46,17 +40,16 @@ axios.get(`https://www.indeed.com/jobs?q=${keywords[1]}%20developer%20${keywords
 				.find('.date')
 				.text();
 				
-			jobs['id' + index] = index;
-			jobs['title' + index] = title;
-			jobs['company' + index] = companyName;
-			jobs['location' + index] = location;
-			jobs['description' + index] = description;
-			jobs['link' + index] = link;
-			jobs['date' + index] = date;
-		});
-	})
+			jobs['id' + i] = i;
+			jobs['title' + i] = title;
+			jobs['company' + i] = companyName;
+			jobs['location' + i] = location;
+			jobs['description' + i] = description;
+			jobs['link' + i] = link;
+			jobs['date' + i] = date;
+	});
 
-	.catch((error) => console.log(error))
-	.then(() => console.log(jobs));
+	console.log(jobs)
+}
 
-
+getJobListings();
