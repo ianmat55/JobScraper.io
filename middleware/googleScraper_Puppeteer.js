b56af08851a2293e36ebdba8dad3c4f3ad65e9a8
ianmat55 <ianmat55@gmail.com>
2021-11-21 16:05:54 -0800

    puppeteer scraper returns titles and links object, no filter function yet

--- middleware/googleScraper_Puppeteer.js
+++ middleware/googleScraper_Puppeteer.js
@@ -1,19 +1,48 @@
 const puppeteer = require('puppeteer');
 
-// example: `https://www.google.com/search?ei=HSq0Xt7IDoz5gQbGip6ABg&q=${techInput}+${locationInput}&oq=${techInput}+${locationInput}+jobs+&gs_lcp=CgZwc3ktYWIQAzIFCCEQoAEyBQghEKABMgUIIRCgATIFCCEQoAE6BggAEAgQHjoGCAAQFhAeUOYSWNIXYIIZaABwAHgAgAGrAYgB7QaSAQM1LjOYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&uact=5&ibp=htl;jobs&sa=X&ved=2ahUKEwiH6N6-iaLpAhX9QUEAHTWjBGMQiYsCKAF6BAgKEBE#fpstate=tldetail&htivrt=jobs&htidocid=odTsQfLDpmatfzNQAAAAAA%3D%3D`,
-
 const templateURL = (title, location) => { 
  	url = `https://www.google.com/search?ei=HSq0Xt7IDoz5gQbGip6ABg&q=${title}+${location}&oq=${title}+${location}+jobs+&gs_lcp=CgZwc3ktYWIQAzIFCCEQoAEyBQghEKABMgUIIRCgATIFCCEQoAE6BggAEAgQHjoGCAAQFhAeUOYSWNIXYIIZaABwAHgAgAGrAYgB7QaSAQM1LjOYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&uact=5&ibp=htl;jobs&sa=X&ved=2ahUKEwiH6N6-iaLpAhX9QUEAHTWjBGMQiYsCKAF6BAgKEBE#fpstate=tldetail&htivrt=jobs&htidocid=odTsQfLDpmatfzNQAAAAAA%3D%3D`
     return url;
 }
 
-async function run() {
+async function run(title, location) {
     const browser = await puppeteer.launch({
         headless: false, // using temporarily to see if its working, ideally we want this func to return json
         defaultViewport: null
     });
     const page = await browser.newPage();
-    await page.goto(templateURL('web developer', 'San Jose, CA USA'));
+    await page.goto(templateURL(title, location));
+
+    // const listings = await page.$('.nJXhWc');
+
+    const titles = await page.evaluate(() =>
+        Array.from(
+            document.querySelectorAll(".BjJfJf"),
+            (element) => element.textContent
+        )
+    );
+
+    // const hrefs = await page.evaluate(() =>
+    //     Array.from(
+    //         document.querySelectorAll(".EDblX"),
+    //         (element) =>
+    //         element.firstElementChild.firstElementChild.firstElementChild.href
+    //     )
+    // );
+
+    const hrefs = await page.evaluate(() =>
+    Array.from(
+        document.querySelectorAll(".pMhGee"),
+        (element) =>
+        element.href
+    )
+);
+
+
+    const listings = {};
+    titles.forEach((title, i) => listings[title] = hrefs[i]);
+
+    console.log(listings);
     // browser.close();
 }
-run();
+run('web developer', 'San Jose, CA USA');
